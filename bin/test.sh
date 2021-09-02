179a22e5b69d9d3fcc193940452d39bb465eac36
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2021-09-02 20:32:24 +0900

    Add test script and GitHub Actions setting

---
+++ bin/test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+readonly SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd)"
+readonly PROJECT_HOME="${SCRIPT_DIR}/.."
+
+readonly MAX_RETRY=30
+readonly TARGET='localhost:8080/api/posts'
+
+cd "${PROJECT_HOME}"
+docker-compose down
+docker-compose up -d
+
+for i in $(seq 1 "${MAX_RETRY}"); do
+  set +o errexit
+  curl -f "${TARGET}"
+  curl_result="$?"
+  set -o errexit
+
+  if [[ "${curl_result}" -eq 0 ]]; then
+    echo 'TEST SUCCEEDED'
+    exit 0
+  fi
+
+  sleep 1
+done
+
+echo 'TEST FAILED'
+exit 1
